7752ca7d1dcf43fb0bfeccf95bbf6488ce6ba540
Daire Grimes <c14407812@mydit.ie>
2017-02-18 20:14:39 +0100

    Attacking Works

--- GameDevProject/Assets/Scripts/Patrol.cs
+++ GameDevProject/Assets/Scripts/Patrol.cs
@@ -15,6 +15,7 @@ public enum State {
 
 public class Patrol : MonoBehaviour {
 
+
 	public Transform[] points;
 	public Transform player;
 	private int destPoint = 0;
@@ -22,7 +23,6 @@ public class Patrol : MonoBehaviour {
 	static Animator anim;
 
 	private int state = 1;
-
 
 
 	void Start () {
@@ -60,8 +60,8 @@ public class Patrol : MonoBehaviour {
 		// Choose the next destination point when the agent gets
 		// close to the current one.
 
-
 		Vector3 direction = player.position - this.transform.position;
+		float angle = Vector3.Angle(direction,this.transform.forward);
 		//float angle = Vector3.Angle(direction,this.transform.forward);
 		switch (state) {
 			
@@ -69,12 +69,13 @@ public class Patrol : MonoBehaviour {
 			{
 				if (agent.remainingDistance < 0.5f) {
 
-					anim.SetBool("isWalking",false);
+					agent.speed = 50;
 					GotoNextPoint ();
 
 				}
 
-				if (Vector3.Distance (player.position, this.transform.position) < 10) {
+				if (Vector3.Distance (player.position, this.transform.position) < 10 && angle < 30) {
+					anim.SetBool("isWalking",false);
 					state = 2;
 
 				}
@@ -85,12 +86,19 @@ public class Patrol : MonoBehaviour {
 
 		case 2: 
 			{
+				agent.speed = 100;
 				anim.SetBool("isRunning",true);
 				direction.y = 0;
 
 				this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
 					Quaternion.LookRotation(direction), 0.1f);
 				agent.destination = player.position;
+
+
+				if (Vector3.Distance (player.position, this.transform.position) < 5 && angle < 30) {
+
+					anim.SetBool("isAttacking",true);
+				}
 
 			}break;
 
