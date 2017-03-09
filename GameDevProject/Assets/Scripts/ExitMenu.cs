280f0ad6afc2e1bf5330d11f5d1e761ba458787f
Daire Grimes <c14407812@mydit.ie>
2017-03-09 18:19:05 +0100

    Start again menu finished

---
+++ GameDevProject/Assets/Scripts/ExitMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
+
+public class ExitMenu : MonoBehaviour {
+
+
+	public Button startText;
+	public Button exitText;
+
+	void Start () {
+
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		
+	}
+	
+	public void StartLevel () //this function will be used on our Play button
+
+	{
+		SceneManager.LoadScene (1); //this will load our first level from our build settings. "1" is the second scene in our game
+	}
+
+	public void ExitGame () //This function will be used on our "Yes" button in our Quit menu
+
+	{
+		Application.Quit(); //this will quit our game. Note this will only work after building the game
+
+	}
+}
